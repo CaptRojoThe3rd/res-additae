69e8c0dfd10f8bc7420f28e914e9786ab2068eaa
CaptRojoThe3rd <littlerojo3@gmail.com>
2026-07-30 17:58:30 -0700

    fix config bug

--- src/main/java/com/captrojo/resadditae/config/CommonConfig.java
+++ src/main/java/com/captrojo/resadditae/config/CommonConfig.java
@@ -29,6 +29,9 @@ public class CommonConfig
 		
 		public static void load()
 		{
+			ModConfig._category = "debug";
+			ModConfig.setCategoryComment("Debugging settings");
+			
 			log_structure_gens = ModConfig.getBool("log_structure_gens", "Log the position of all generated structures and notable features", log_structure_gens);
 			log_failed_structure_gens = ModConfig.getBool("log_failed_structure_gens", "Log the positions of structures that did not generate. This does not affect the reporting of errors", log_failed_structure_gens);
 		}
